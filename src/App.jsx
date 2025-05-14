b1f39599c855299e115fd568cdbb3348efe9f790
SRB77 <soumyach144@gmail.com>
2025-05-14 09:42:24 +0530

    password generator project, setup the whole react project with vite , and then added tailwind initial commit .

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import {React, useState } from 'react'
+function App() {
+  return (
+    <>
+        <div className="w-full max-w-md mx-auto shadow-md rounded-lg  px-4 my-8 bg-gray-700 text-orange-500">test</div>
+    </>
+  );
+}
+
+export default App
