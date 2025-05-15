4b3aa6fe805d9ae84377ed08c7567909bc2da713
SRB77 <soumyach144@gmail.com>
2025-05-15 10:24:13 +0530

    Styling done , and password generator function also done .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,73 @@
 /* eslint-disable no-unused-vars */
-import {React, useState } from 'react'
+import { React, useEffect, useState } from "react";
+import "./App.css";
+
 function App() {
+  const [length ,setLength] = useState(8);
+  const [numAllow ,setnumAllow] = useState(false);
+  const [charAllow ,setcharAllow] = useState(false);
+  let [Password ,setPassword] = useState("");
+
+  useEffect(()=>{
+    const passwordGenerator = () => {
+      let aplhabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      let numbers = "0123456789";
+      let specialChar = "~!@#$%^&*<>.?/";;
+      let pass = "";
+      for (let i = 1; i <=length; i++) {
+        let index = Math.floor(Math.random() * length + 1);
+        pass += aplhabet.charAt(index);
+      }
+      setPassword(pass);
+    };
+    passwordGenerator()
+  },[])
+  console.log(Password)
+
   return (
     <>
-        <div className="w-full max-w-md mx-auto shadow-md rounded-lg  px-4 my-8 bg-gray-700 text-orange-500">test</div>
+      <div className="main">
+        <div className="container">
+          <h1 className="Heading">Password Generator</h1>
+          <div className="input-div">
+            <input
+              type="text"
+              className="input-box"
+              placeholder="PASSWORD HERE "
+              value={Password}
+              readOnly
+            />
+            <button className="copyBtn">COPY</button>
+          </div>
+
+          <div className="param-div">
+            <label>
+              <input
+                type="range"
+                name="input-range"
+                className="input-range"
+                min={6}
+                max={24}
+                defaultValue={8}
+              />
+              :length
+            </label>
+
+            <label>
+              <input type="checkbox" className="checkbox"/>
+              :Numbers
+            </label>
+
+            <label>
+              <input type="checkbox" className="checkbox"/>
+              :SpecialChar
+            </label>
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
